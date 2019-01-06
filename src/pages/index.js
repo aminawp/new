93eaf30b7c46d55ec656eba6948c5ec1f9917431
aminawp <amina.saadwp@gmail.com>
2019-01-06 10:36:33 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>I am a web Guru</h1>
+    <p>Welcome to my portfolio.</p>
+    <p> I am building great web prducts.</p>
+    <p> Trendy UX UI</p>
+    <p> Trendy Design</p>
+    <Link to="/page-2/">Go to page 8</Link>
+  </div>
+)
+
+export default IndexPage
