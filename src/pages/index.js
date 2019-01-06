59710b29a46e547d1b01d7dba6eaaaf7727aaf55
aminawp <amina.saadwp@gmail.com>
2019-01-06 11:00:38 -0500

    in

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>I am a web Guru</h1>
     <p>Welcome to my portfolio.</p>
-    <p> I am building great web prducts.</p>
+    <p> I am buiding great web prducts.</p>
     <p> TrendUI</p>
     <p> Trendy Design</p>
     <Link to="/page-2/">G to pe 8</Link>
